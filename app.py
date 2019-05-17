51f6ac384242f2e1d9b64b0cbe96e0b62bf8baa1
Said Aitmbarek <saitmbarek@aldebaran-robotics.com>
2019-05-17 16:17:13 +0200

    remove dependency on seaborn.

--- app.py
+++ app.py
@@ -9,16 +9,17 @@ import pandas as pd
 import numpy as np
 import re
 import json
-import seaborn as sns
 import dash_dangerously_set_inner_html
 
 ## globals: FIXME: load from conf
 MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiamFja3AiLCJhIjoidGpzN0lXVSJ9.7YK6eRwUNFwd3ODZff6JvA"
 N_BINS = 10
 SCALE = 100
-_palette = sns.light_palette((5, 90, 55), N_BINS, input="husl")
-DEFAULT_COLORSCALE = _palette.as_hex()
-# cm = dict(zip(BINS, DEFAULT_COLORSCALE))
+
+DEFAULT_COLORSCALE = [
+    u'#fee7ec', u'#fdd3dd', u'#fcbfcd', u'#fbabbd', u'#fb97ae',
+    u'#fa839e', u'#f96f8f', u'#f85b7f', u'#f7476f', u'#f63360'
+]
 DEFAULT_OPACITY = 0.8
 
 ## helpers:
